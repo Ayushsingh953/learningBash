339c5743ea00ae3683a235cdd85965140e1b64b6
ayushsingh953 <ayushsingh35323@gmail.com>
2024-07-07 14:16:46 +0530

    practice

---
+++ practice.sh
@@ -0,0 +1,9 @@
+# help let
+# $((NUMBER+4))
+# echo $((NUMBER+4))
+# assign value after calc
+# J=$(($I-6))
+# (( ... )) will perform a calculation or operation and output nothing. $(( ... )) will replace the calculation with the result of it.
+# create an array - ARR=("a","b","c")
+# print elements - echo ${ARR[1]}
+# print all elements - echo ${ARR[@]} or echo ${ARR[*]}
