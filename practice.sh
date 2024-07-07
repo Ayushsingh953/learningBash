76d40048e3aefa0c9799ce89b85becd6a62457f7
ayushsingh953 <ayushsingh35323@gmail.com>
2024-07-07 15:23:46 +0530

    practice

--- practice.sh
+++ practice.sh
@@ -8,3 +8,5 @@
 # print elements - echo ${ARR[1]}
 # print all elements - echo ${ARR[@]} or echo ${ARR[*]}
 # pattern matching - [[ hello =~ el ]]; echo $?
+
+# way to set the stdin is by using the pipe (|). It will use the output from one command as input for another. Here's an example: <command_1> | <command_2>. This will take the stdout from command_1 and use it as the stdin for command_2
